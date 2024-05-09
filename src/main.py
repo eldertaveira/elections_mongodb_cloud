5fffe52ad80041a9d6e3f4e4f11530af6d11c6fb
Matheus Oliveira <matheus.oliveira.residente@tre-rn.jus.br>
2024-05-09 16:05:56 -0300

    feat: ensure the zip is downloaded and unzipped only when does not exists

--- src/main.py
+++ src/main.py
@@ -1,41 +1,33 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
+import os
+from urllib import request
 
 def import_mongo():
-
-
-    # client = MongoClient("34.28.35.28", 80)
-    # client = MongoClient(uri)
-    # db = client["elections_db"]
-    # collection = db["brasil"]
     url = 'https://cdn.tse.jus.br/estatistica/sead/odsele/votacao_candidato_munzona/votacao_candidato_munzona_2022.zip'
     work_dir = "./work/"
     destination_file = "votacao_candidato_munzona_2022.zip"
+    source_csv_filename = 'votacao_candidato_munzona_2022_BRASIL.csv'
+    extracted_file = os.path.join(work_dir, 'extracted', source_csv_filename)
 
-    from urllib import request
-    import os
-
-    # Create folder
     folder_is_created = os.path.exists(work_dir)
+    file_is_extracted = os.path.exists(extracted_file)
 
     if not folder_is_created:
       os.mkdir(work_dir)
 
-    destination_zip_path = os.path.join(work_dir, destination_file)
+    if not file_is_extracted:
+        destination_zip_path = os.path.join(work_dir, destination_file)
 
-    # Download the file from `url` and save it locally
-    # request.urlretrieve(url, destination_zip_path)
+        # Download the file from `url` and save it locally
+        request.urlretrieve(url, destination_zip_path)
 
-    from zipfile import ZipFile
-    import os
+        from zipfile import ZipFile
 
-    source_csv_filename = 'votacao_candidato_munzona_2022_BRASIL.csv'
+        with ZipFile(destination_zip_path, 'r') as zip_file:
+            source_csv_directory_path = os.path.join(work_dir, 'extracted')
+            zip_file.extract(source_csv_filename, source_csv_directory_path)
 
-    with ZipFile(destination_zip_path, 'r') as zip_file:
-        source_csv_directory_path = os.path.join(work_dir, 'extracted')
-        # zip_file.extract(source_csv_filename, source_csv_directory_path)
-
-    extracted_file = os.path.join(work_dir, 'extracted', source_csv_filename)
 
     host = "34.28.35.28"
     port = 80
