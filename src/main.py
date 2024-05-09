88a50808373c2067a8481818b2b39a6bb5002be2
matheusinit <matheus.oliveira.s@protonmail.com>
2024-05-09 18:40:29 -0300

    feat: create database and collection when does not exists

--- src/main.py
+++ src/main.py
@@ -3,10 +3,40 @@ from pyspark.sql.dataframe import DataFrame
 import os
 from urllib import request
 from typing import TypedDict
+import pymongo
+import traceback
 
 EXTRACTION_FOLDER_PATH = './work/'
 
 def load_data_into_mongodb():
+    username = "root"
+    password = "example"
+    host = "localhost"
+
+    database_name = "elections"
+    collection_name = "general_elections_2022"
+
+    data_lake_connection_string = create_connection_string({
+        'username': username,
+        'password': password,
+        'host': host,
+        'port': 80,
+        'collection': None,
+        'database': None
+    })
+
+    data_warehouse_connection_string = create_connection_string({
+        'username': username,
+        'password': password,
+        'host': host,
+        'port': 443,
+        'collection': None,
+        'database': None
+    })
+
+    create_mongodb_database_and_collection(data_lake_connection_string, database_name, collection_name)
+    create_mongodb_database_and_collection(data_warehouse_connection_string, database_name, collection_name)
+
     csv_file_path = get_path_for_csv_file()
     check_if_extraction_folder_is_created(EXTRACTION_FOLDER_PATH)
 
@@ -35,6 +65,27 @@ def load_data_into_mongodb():
     df.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
 
     spark_session.stop()
+
+def create_mongodb_database_and_collection(url: str, database_name: str, collection_name: str):
+    try:
+        client = pymongo.MongoClient(url)
+
+        if database_name in client.list_database_names():
+            return
+
+        database = client[database_name]
+
+        if collection_name in database.list_collection_names():
+            return
+
+        database[collection_name].insert_one({"example": 'example'})
+        database[collection_name].delete_many({})
+
+        if collection_name in database.list_collection_names():
+            print(f"Database '{database_name}' and Collection '{collection_name}' created successfully.")
+    except Exception:
+        print(f"Failed to create database {database_name} or collection '{collection_name}'. Exception:")
+        traceback.print_exc()
 
 
 def get_path_for_csv_file():
@@ -83,16 +134,20 @@ class ConnectionOptions(TypedDict):
     port: int
     username: str
     password: str
-    database: str
-    collection: str
+    database: str | None
+    collection: str | None
 
 def create_connection_string(options: ConnectionOptions):
     username = options['username']
     password = options['password']
     host = options['host']
     port = options['port']
-    database = options['database']
+    database = options['collection']
     collection = options['collection']
+
+    if database == None and collection == None:
+        connection_string = f"mongodb://{username}:{password}@{host}:{port}"
+        return connection_string
 
     connection_string = f"mongodb://{username}:{password}@{host}:{port}/{database}.{collection}"
 
